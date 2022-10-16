a077676a80183ef24386b9015ef28e0aadcbc95d
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2022-10-16 09:02:48 -0300

    Chromatic struture created.

--- stories/Pagination.stories.tsx
+++ stories/Pagination.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { PaginationControl, PaginationControlProps } from '../src';
+import { useArgs } from '@storybook/addons';
 
 const meta: Meta = {
-  title: 'React Bootstrap Pagination',
+  title: 'React Bootstrap Pagination Control',
   component: PaginationControl,
   parameters: {
     controls: { expanded: true },
@@ -12,17 +13,16 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<PaginationControlProps> = args => <PaginationControl {...args} />;
+const Template: Story<PaginationControlProps> = args => {
+  const [{ page }, updateArgs] = useArgs()
+  return <PaginationControl page={page} changePage={number => updateArgs({ page: number })} {...args} />
+};
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
 
 Default.args = {
-  page: 1,
   total: 250,
-  limit: 30,
-  changePage: (number) => {
-    console.log("Change to page " + number)
-  }
+  limit: 30
 };
